003ad3745daf472d1ab5ad6d422c1f6a164913d9
mengyu12 <42936633+mengyu12@users.noreply.github.com>
2019-04-02 17:30:23 +0800

    上传个人描述的设计
    上传个人介绍

---
+++ 个人网页设计01/紫色例子/WebContent/js/js1.js
@@ -0,0 +1,6 @@
+document.getElementById("inner_menu1").onmouseover=function(){
+	document.getElementById("inner_menu").style.display="inline";
+};
+document.getElementById("inner_menu1").onmouseout=function(){
+	document.getElementById("inner_menu").style.display="block";
+};
